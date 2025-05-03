b05570b92d1229026c4545234bfc5524b88560a9
vluend <vluendy@gmail.com>
2025-05-03 20:05:01 -0300

    anadi suma ggg

--- testfile.py
+++ testfile.py
@@ -1,3 +1,9 @@
 
 
 print('Hola Den')
+
+#vamos a hacer una suma por input de 2 nros
+a = [0, 0]
+for i in range(2):
+    a[i] = int(input('ingrese un numero: \t'))
+print(f'La suma es:\t{a[0]+a[1]}')
